167acb1962f7d5561a0b0613c9b84bdb12fa553c
nawla <nawlawtisthebest@gmail.com>
2024-04-15 17:56:54 +0630

    4/15/2024(mon)

---
+++ app.js
@@ -0,0 +1,25 @@
+// // Get UI
+// const inputs = document.querySecectorAll('input');
+// // console.log(inputs);
+// let datas = [];
+
+// inputs.forEach(function(input){
+// 	// console.log(input);
+
+// 	inputs.addEventListener('keyup',function(){
+// 		//console.log(this.value);
+
+// 		datas.push({
+// 			digit:inputs[0].value,
+// 			name:inputs[1].value,
+// 			exp:inputs[2].value,
+// 			ccv:inputs[3].value
+// 		});
+
+// 		console.log(datas)
+// 	}
+// })
+// 	// 8CV
+
+
+console.log("hi");
